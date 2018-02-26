30e21aa99b458af77cb4025aeabe60388c9dbf85
Tomash <tomash@MacBook-Pro-Andrii.local>
2018-02-26 19:44:14 +0200

    init

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <View style={{padding: 30, flex: 1}}>
+       
+         <Text>gg</Text> 
+        
+      </View>
+    );
+  }
+}
+
